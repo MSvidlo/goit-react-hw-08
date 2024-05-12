d2e5b5a2cbac9f70d037c7bdea52828d3f9b92ae
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-05-12 09:42:38 +0200

    add some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+
 export const HomePage = lazy(() => import('./HomePage/HomePage.jsx'));
 export const RegisterPage = lazy(() => import('./RegisterPage/RegisterPage.jsx'));
 export const LoginPage = lazy(() => import('./LoginPage/LoginPage.jsx'));
